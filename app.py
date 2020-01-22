89f13d5344a76cd8bda281986800707f431a6e5d
Krishduino <38496983+Krishduino@users.noreply.github.com>
2020-01-22 18:13:28 +0200

    tested and functional

--- app.py
+++ app.py
@@ -6,13 +6,29 @@
 #  * @Title - FLASK WEBAPP DEMO
 #  */
 
+#imports
 from flask import Flask
+from flask import render_template
 
+#creating application object
 app = Flask(__name__)
 
+#decorators to link functions to urls
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
 
+@app.route('/new_route')
+def new_route():
+    return render_template('new_route.html')
+
+#dynamic route page testing
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('page.html', name=name)
+
+
+
+# start the development server using the run() method
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
